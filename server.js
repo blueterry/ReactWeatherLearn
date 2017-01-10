fd032dac4858b83da0cb981a8db03d8e794e959f
Terry <blueterry@yahoo.com>
2017-01-10 23:12:26 +0800

    Prep repo for Heroku 2

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ var express = require('express');
 //Create our app
 var app = express();
 const THE_PORT = process.env.PORT || 3000;
-/*
+
 app.use(function(req, res, next){
-    if(req.headers['x-forworded-proto'] === 'http'){
+    if(req.headers['x-forwarded-proto'] === 'http'){
         next();
-    }else{
+    } else{
         res.redirect('http://' + req.hostname + req.url);
     }
 });
-*/
+/**/
 app.use(express.static('public'));
 
 app.listen(THE_PORT, function(){
