e97eed07a65f307f79170b05143468375d9894d2
Terry <blueterry@yahoo.com>
2017-01-10 23:08:32 +0800

    Modified to remove redirect

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express');
 
 //Create our app
 var app = express();
-const PORT = process.env.PORT || 3000;
-
+const THE_PORT = process.env.PORT || 3000;
+/*
 app.use(function(req, res, next){
     if(req.headers['x-forworded-proto'] === 'http'){
         next();
@@ -11,9 +11,9 @@ app.use(function(req, res, next){
         res.redirect('http://' + req.hostname + req.url);
     }
 });
-
+*/
 app.use(express.static('public'));
 
-app.listen(PORT, function(){
-    console.log('Express server is up on port: ' + PORT);
+app.listen(THE_PORT, function(){
+    console.log('Express server is up on port: ' + THE_PORT);
 });
