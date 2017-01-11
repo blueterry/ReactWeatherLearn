7db45dc3c90f56a28bf670cde0e33c8c1f1fad68
Terry <blueterry@yahoo.com>
2017-01-11 10:54:02 +0800

    Sytle Nav bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -3,14 +3,37 @@ var {Link, IndexLink} = require('react-router');
 //IndexLink 对应 IndexRoute
 
 var Nav = React.createClass({
+    onSearch :function (e){
+        e.preventDefault();
+        alert('Not yet wired up!');
+    },
     render: function(){
         return (
-            <div>
-                <h3>Nav1</h3>
-                <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Weather</IndexLink>
-                <Link to="about" activeClassName="active" activeStyle={{fontWeight:'bold'}}>About</Link>
-                <Link to="examples" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Examples</Link>
-                
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="menu">
+                        <li className="menu-text">React Weather App</li>
+                        <li>
+                            <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Weather</IndexLink>
+                        </li>
+                        <li>
+                            <Link to="about" activeClassName="active" activeStyle={{fontWeight:'bold'}}>About</Link>
+                        </li>
+                        <li>
+                            <Link to="examples" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Examples</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="top-bar-right">
+                    <form action="" onSubmit={this.onSearch}>
+                        <ul className="menu">
+                            <li>
+                                <input type="search" placeholder="Search Weather" />
+                            </li>
+                            <li><input type="submit" className="button" value="Get Weather"/></li>
+                        </ul>                    
+                    </form>
+                </div>
             </div>
         );
     }
